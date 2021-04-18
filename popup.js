a6c36b88419277e2d1340f9fd3fea4a4ed03fc38
Mohammad Moiz Ali <38657358+moizalikhan119@users.noreply.github.com>
2021-04-18 14:16:24 +0500

    GP 1.0.1
    Some Additional Feature Added to the plugin

--- popup.js
+++ popup.js
@@ -1,17 +1,95 @@
 document
 	.getElementById('generate')
 	.addEventListener('click', function makePassword() {
-		var collectionOfLetters =
-			'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&';
-		var generatedPassword = '';
-		var size = collectionOfLetters.length;
-		for (var i = 0; i < 12; ++i) {
+		let collectionOfAlphabetsCapital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		let generatedcollectionOfAlphabetsCapital = '';
+		let collectionOfAlphabetCapitalSize = collectionOfAlphabetsCapital.length;
+		for (let i = 0; i < 12; ++i) {
+			generatedcollectionOfAlphabetsCapital =
+				generatedcollectionOfAlphabetsCapital +
+				collectionOfAlphabetsCapital.charAt(
+					Math.floor(Math.random() * collectionOfAlphabetCapitalSize)
+				);
+		}
+		let collectionOfAlphabetsSmalls = 'abcdefghijklmnopqrstuvwxyz';
+		let generatedcollectionOfAlphabetsSmalls = '';
+		let collectionOfAlphabetsSmallSize = collectionOfAlphabetsSmalls.length;
+		for (let i = 0; i < 12; ++i) {
+			generatedcollectionOfAlphabetsSmalls =
+				generatedcollectionOfAlphabetsSmalls +
+				collectionOfAlphabetsSmalls.charAt(
+					Math.floor(Math.random() * collectionOfAlphabetsSmallSize)
+				);
+		}
+		let collectionOfNumbers = '0123456789';
+		let generatedcollectionOfNumbers = '';
+		let collectionOfNumberSize = collectionOfNumbers.length;
+		for (let i = 0; i < 12; ++i) {
+			generatedcollectionOfNumbers =
+				generatedcollectionOfNumbers +
+				collectionOfNumbers.charAt(
+					Math.floor(Math.random() * collectionOfNumberSize)
+				);
+		}
+		let collectionOfSymbols = '!@#$%^&*/';
+		let generatedcollectionOfSymbols = '';
+		let collectionOfSymbolSize = collectionOfSymbols.length;
+		for (let i = 0; i < 12; ++i) {
+			generatedcollectionOfSymbols =
+				generatedcollectionOfSymbols +
+				collectionOfSymbols.charAt(
+					Math.floor(Math.random() * collectionOfSymbolSize)
+				);
+		}
+		let combinationOfAlphabets = generatedcollectionOfAlphabetsCapital.concat(
+			'',
+			generatedcollectionOfAlphabetsSmalls
+		);
+		let combinationOfAlphabetsAndNumber = combinationOfAlphabets.concat(
+			'',
+			generatedcollectionOfNumbers
+		);
+		let combinationOfAlphabetsAndSymbols = combinationOfAlphabets.concat(
+			'',
+			generatedcollectionOfSymbols
+		);
+		let combination = combinationOfAlphabetsAndNumber.concat(
+			'',
+			generatedcollectionOfSymbols
+		);
+		let collectionOfLetters;
+		let number = document.getElementById('number').value;
+		if (number === '') {
+			number = '12';
+		}
+		let specialCharacter = document.getElementById('specialCharacter');
+		let numberIncluded = document.getElementById('numberIncluded');
+		if (specialCharacter.checked === true && numberIncluded.checked === true) {
+			collectionOfLetters = combination;
+		} else if (
+			specialCharacter.checked === false &&
+			numberIncluded.checked === true
+		) {
+			collectionOfLetters = combinationOfAlphabetsAndNumber;
+		} else if (
+			specialCharacter.checked === true &&
+			numberIncluded.checked === false
+		) {
+			collectionOfLetters = combinationOfAlphabetsAndSymbols;
+		} else if (
+			specialCharacter.checked === false &&
+			numberIncluded.checked === false
+		) {
+			collectionOfLetters = combinationOfAlphabets;
+		}
+		let generatedPassword = '';
+		let size = collectionOfLetters.length;
+		for (let i = 0; i < number; ++i) {
 			generatedPassword =
 				generatedPassword +
 				collectionOfLetters.charAt(Math.floor(Math.random() * size));
 		}
 		let password = document.getElementById('password');
 		password.innerText = generatedPassword;
-		console.log('click');
 		return password;
 	});
